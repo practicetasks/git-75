a24e265182f2bc21c81a47aa5fb1688acb8324d5
= <=>
2024-01-12 19:48:21 +0600

    Version 3

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,17 @@ import test.Data;
 public class Main {
     public static void main(String[] args) {
         System.out.println("тест");
+
+        // git init
+        // git add src
+        // git commit -m "текст"
+
+        // feature: добавлен возможность оставлять отзывы
+        // fix: исправлен баг N 190
+        // refactoring: улучшен код в классе Entity
+        // style:
+
+
+        // git add src/arrays/drink_task
     }
 }
